9346ad74aa7433196ed72a90fb037ede2842ec09
serhattastan <serhattastannn@gmail.com>
2024-09-03 15:55:00 +0300

    Add FloatingActionButton with custom icon and update app icon
    - Added a FloatingActionButton (FAB) to the HomeScreen with a custom drawable icon (R.drawable.icon_plus).
    - Ensured the FAB has a circular shape for consistent design.
    - Updated the application icon to a new design.

--- app/src/main/java/com/cloffygames/techcareerbasicclonetask/uix/view/HomeScreen.kt
+++ app/src/main/java/com/cloffygames/techcareerbasicclonetask/uix/view/HomeScreen.kt
@@ -9,17 +9,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CameraEnhance
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Message
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
@@ -36,9 +41,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.cloffygames.techcareerbasicclonetask.R
 import com.cloffygames.techcareerbasicclonetask.ui.theme.*
 import com.cloffygames.techcareerbasicclonetask.uix.view.items.ProductItem
 import com.cloffygames.techcareerbasicclonetask.uix.view.items.SearchBar
@@ -112,6 +119,25 @@ fun HomeScreen(homeViewModel: HomeViewModel, navController: NavController) {
                     },
                     colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = CustomLightBlue)
                 )
+            },
+            // Floating Action Button
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {
+                        // Butona tıklama işlemi
+
+                    },
+                    containerColor = CustomLightBlue, // FAB'in arka plan rengi
+                    shape = CircleShape,
+                    elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 6.dp)) {
+                    // FAB içinde yerel görsel ikonu
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_plus),
+                        contentDescription = "Ekle",
+                        tint = Color.White,
+                        modifier = Modifier.size(18.dp)
+                    )
+                }
             }
         ) { paddingValues ->
             // Ana içerik alanı, LazyColumn ile oluşturuluyor.
